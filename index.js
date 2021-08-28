3638c36af3ed670860cb2d8e84c759bf5317d80a
giannurwana19 <giannurwana19@gmail.com>
2021-08-28 15:10:25 +0700

    setup server & mongoose

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('ok');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${process.env.APP_URL}/${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
