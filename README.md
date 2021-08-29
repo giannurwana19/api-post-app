e18721d803ae59cd650dabf3b4feb16298b41792
giannurwana19 <giannurwana19@gmail.com>
2021-08-29 23:02:57 +0700

    Update README & comment endpoint user

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# REST API for project react-post-app
+## REST API for project `react-post-app`
+
+Project Post memory App
+
+### Build With :
+
+- NodeJS
+- ExpressJS
+- MongoDB
+- Heroku
+- JSON WEB TOKEN (JWT)
+
+Available on [api-post-app.herokuapp.com](https://api-post-app.herokuapp.com)
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const { signin, signup } = require('../controllers/users');
 const User = require('../models/User');
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
-  const users = await User.find();
+// router.get('/', async (req, res) => {
+//   const users = await User.find();
 
-  return res.json({ data: users });
-});
+//   return res.json({ data: users });
+// });
 
 router.post('/signin', signin);
 router.post('/signup', signup);
